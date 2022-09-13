4c5e2d41315c6b894d55c8f1e76803c5f99d6fcc
bpmcdonell <bpmcdonell@gmail.com>
2022-09-13 17:34:09 -0400

    more js

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,39 @@
-var topics = ['HTML', 'CSS', 'Git', 'CSS']
-for (var ArrayCounter = 0; ArrayCounter < topics.length; ArrayCounter++) {
-    console.log(topics[ArrayCounter]);
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+var x = 0
 
+
+function ListTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-/*
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
+
+
+function SelectTopic() {
+    if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+
+    } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+
+    } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+
+    } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+
+    } else {
+    console.log('Please try again!');
+
+    }
 }
-*/
+
+console.log('Here are the topics we learned through prework:');
+ListTopics();
+
+console.log('Which topic should we study first?');
+SelectTopic();
+
+console.log(randomTopic)
+
