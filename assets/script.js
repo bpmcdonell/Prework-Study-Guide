439aa00f183f5b4114608de7cb08b3d68cf6aaea
bpmcdonell <bpmcdonell@gmail.com>
2022-09-13 16:39:42 -0400

    first touches of Java

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topics = ['HTML', 'CSS', 'Git', 'CSS']
+for (var ArrayCounter = 0; ArrayCounter < topics.length; ArrayCounter++) {
+    console.log(topics[ArrayCounter]);
+
+}
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
